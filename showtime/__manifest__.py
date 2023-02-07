ba95153fe0a4b29d23e697b1e14bd9c36d3d68f9
Harsh Bhatt (habh) <habh@odoo.com>
2023-02-07 19:09:19 +0530

    showtime.venue model created

---
+++ showtime/__manifest__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+{
+    'name':"ShowTime",
+    'depends':['base'],
+    'license':"LGPL-3",
+    'application':True
+}
---
+++ showtime/models/showtime_venue.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from odoo import fields,models
+
+class ShowtimeVenue(models.Model):
+    _name="showtime.venue"
+    _description="ShowTime Venue Description"
+
+    name=fields.Char(required=True)
+    description=fields.Text()
