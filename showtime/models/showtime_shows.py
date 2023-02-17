a1ee8b440fa14f377f617ea5a0827a26d9ec024b
Harsh Bhatt (habh) <habh@odoo.com>
2023-02-17 18:30:24 +0530

    Demo Data Added

--- showtime/models/showtime_shows.py
+++ showtime/models/showtime_shows.py
@@ -2,6 +2,15 @@
 from odoo import fields,models,api
 from dateutil.relativedelta import relativedelta
 
+RATING = [
+    ('0','No Rating'),
+    ('1', 'Terrible'),
+    ('2', 'Bad'),
+    ('3', 'Average'),
+    ('4', 'Good'),
+    ('5','Great')
+]
+
 class ShowtimeShows(models.Model):
     _name="showtime.shows"
     _description="ShowTime Shows Description"
@@ -15,6 +24,7 @@ class ShowtimeShows(models.Model):
     end_time = fields.Datetime()
     ticket_ids = fields.One2many("showtime.tickets","show_id",string="Tickets")
     show_type = fields.Selection(string="Type of Seating",selection=[("open","Open Seating"),("alloted","Alloted Seating")],default="alloted")
+    rating = fields.Selection(RATING,string="Rating")
         
     @api.depends("section_id")
     def _compute_venue(self):
